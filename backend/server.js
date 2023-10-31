cf047e16f59863e2d7b9e60e6ab6700eaecfafb3
Syed-Javith <syedjavith14@gmail.com>
2023-10-31 16:21:23 +0530

    publish

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectToDB();
 app.use(bodyParserMiddleware)
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://rsjchatapp.netlify.app'],
   credentials: true, 
 }))
 
